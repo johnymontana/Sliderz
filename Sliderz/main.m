862a8287e34d5de0c92b978c14c154adfdf244b5
johnymontana <lyonwj@gmail.com>
2012-08-29 07:06:56 -0600

    Initial Commit

---
+++ Sliderz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Sliderz
+//
+//  Created by lyonwj on 8/29/12.
+//  Copyright (c) 2012 lyonwj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
